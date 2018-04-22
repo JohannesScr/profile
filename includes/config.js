66d8a6189e00cb3bea8b101ec287afc1dd1f13b7
Johannes Scribante <johannesscr@Johanness-MacBook.local>
2018-04-22 11:46:39 +0200

    fix - heroku deplot

--- includes/config.js
+++ includes/config.js
@@ -1,15 +1,15 @@
 const setup_environment = () => {
-    // let env = process.env.ENV || 'development';
-    //
-    // if (env === 'development' || env === 'test') {
-    //     let config = require('./config.json');
-    //     config = config[env];
-    //
-    //     let keys = Object.keys(config);
-    //     keys.forEach(key => {
-    //         process.env[key] = config[key];
-    //     });
-    // }
+    let env = process.env.ENV || 'development';
+
+    if (env === 'development' || env === 'test') {
+        let config = require('./config.json');
+        config = config[env];
+
+        let keys = Object.keys(config);
+        keys.forEach(key => {
+            process.env[key] = config[key];
+        });
+    }
 };
 
 module.exports = {
