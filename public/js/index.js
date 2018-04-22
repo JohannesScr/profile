5c58e0c11022f8e0e555212c92c9ad891053fc2f
Johannes Scribante <johannesscr@Johanness-MacBook.local>
2018-04-22 12:46:59 +0200

    feat - menu nav toggle for mobile

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,9 @@
 document.getElementById('title').innerText = 'JS text';
+
+let menu_default = document.getElementById('menu_default');
+
+menu_default.addEventListener('click', (event) => {
+    event.preventDefault();
+    let main_menu = document.getElementById('main_menu');
+    main_menu.classList.toggle('show-block');
+});
