9279fdfb00c1b75349c618d9687d58121cc43892
Johannes Scribante <johannesscr@Johanness-MacBook.local>
2018-05-02 23:55:41 +0200

    fix - nav js fixed and added to seperate file as nav.js

--- public/js/index.js
+++ public/js/index.js
@@ -3,43 +3,6 @@
 // VARIABLES
 let wl = document.getElementsByClassName('bg-wl');
 
-function menu(event) {
-    event.preventDefault();
-    // console.log(event.target.name);
-    let submenu = document.getElementById(`menu_${event.target.name}`);
-    submenu.classList.toggle('show-block');
-}
-
-let previous_submenu = null;
-
-function submenu(event) {
-    event.preventDefault();
-
-    let submenu = document.getElementById(`submenu_${event.target.name}`);
-
-    if (previous_submenu === `submenu_${event.target.name}`) {
-        submenu.classList.toggle('hide-block');
-    } else {
-
-        let open = document.querySelectorAll('.nav-submenu');
-        open.forEach(element => {
-            for (let i = 0; i < element.classList.length; i++) {
-                let list = element.classList.value.split(' ');
-                let boolean = list.filter(class_name => {
-                    return class_name === 'hide-block';
-                });
-
-                if (boolean.length === 0) {
-                    element.classList.add('hide-block');
-                }
-            }
-        });
-
-        submenu.classList.toggle('hide-block');
-        previous_submenu = `submenu_${event.target.name}`;
-    }
-}
-
 function section_1_bg() {
     let section_1_height = document.getElementsByClassName('section-1')[0].clientHeight;
     wl = document.getElementsByClassName('bg-wl');
---
+++ public/js/nav.js
@@ -0,0 +1,36 @@
+function menu(event) {
+    event.preventDefault();
+    // console.log(event.target.name);
+    let submenu = document.getElementById(`menu_${event.target.name}`);
+    submenu.classList.toggle('show-block');
+}
+
+let previous_submenu = null;
+
+function submenu(event) {
+    event.preventDefault();
+
+    let submenu = document.getElementById(`submenu_${event.target.name}`);
+
+    if (previous_submenu === `submenu_${event.target.name}`) {
+        submenu.classList.toggle('hide-block');
+    } else {
+
+        let open = document.querySelectorAll('.nav-submenu');
+        open.forEach(element => {
+            for (let i = 0; i < element.classList.length; i++) {
+                let list = element.classList.value.split(' ');
+                let boolean = list.filter(class_name => {
+                    return class_name === 'hide-block';
+                });
+
+                if (boolean.length === 0) {
+                    element.classList.add('hide-block');
+                }
+            }
+        });
+
+        submenu.classList.toggle('hide-block');
+        previous_submenu = `submenu_${event.target.name}`;
+    }
+}
